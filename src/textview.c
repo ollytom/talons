5f4dd422f5690319509ed04e9454b3cf4bb96330
Oliver Lowe <o@olowe.co>
2025-08-27 18:14:19 +1000

    Remove unused show contact pic function

--- src/textview.c
+++ src/textview.c
@@ -1723,36 +1723,6 @@ void textview_show_icon(TextView *textview, const gchar *stock_id)
 	return;
 }
 
-static void textview_save_contact_pic(TextView *textview)
-{
-	MsgInfo *msginfo = textview->messageview->msginfo;
-	gchar *filename = NULL;
-	GError *error = NULL;
-	GdkPixbuf *picture = NULL;
-
-	if (!msginfo->extradata || !msginfo->extradata->avatars)
-		return;
-
-	if (textview->avatar_type > AVATAR_FACE)
-		return;
-
-	if (textview->image)
-		picture = gtk_image_get_pixbuf(GTK_IMAGE(textview->image));
-
-	filename = addrindex_get_picture_file(msginfo->from);
-	if (!filename)
-		return;
-	if (!is_file_exist(filename)) {
-		gdk_pixbuf_save(picture, filename, "png", &error, NULL);
-		if (error) {
-			g_warning("failed to save image: %s",
-					error->message);
-			g_error_free(error);
-		}
-	}
-	g_free(filename);
-}
-
 static void textview_show_header(TextView *textview, GPtrArray *headers)
 {
 	GtkTextView *text = GTK_TEXT_VIEW(textview->text);
